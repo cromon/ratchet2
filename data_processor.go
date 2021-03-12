e05a3183401205200d55b12b3b10f3170ba2089d
cromon <e@cromon.co.uk>
2021-03-12 20:57:17 +0000

    Accidental paste, fixed

--- data_processor.go
+++ data_processor.go
@@ -14,28 +14,7 @@ import (
 type DataProcessor interface {
 	// ProcessData will be called for each data sent from the previous stage.
 	// ProcessData is called with a data.JSON instance, which is the data being received,
-	// an outputChan, which is the channel to sThe MIT License (MIT)
-
-Copyright (c) 2016 Daily Burn
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-end data to, and a killChan,
+	// an outputChan, which is the channel to send data to, and a killChan,
 	// which is a channel to send unexpected errors to (halting execution of the Pipeline).
 	ProcessData(d data.JSON, outputChan chan data.JSON, killChan chan error)
 
